fbd6997617acc135da9109930c038aaed7e39dc3
Eugen Wintersberger <eugen.wintersberger@gmail.com>
2011-12-18 21:42:57 +0100

    Integrated changelog generation into the "debian" scons target.

--- smod/__init__.py
+++ smod/__init__.py
@@ -2,4 +2,5 @@ from version import ProgramVersion
 from version import GCCVersionParser
 from version import DoxyVersionParser
 from program import CheckProgram
+from changelog import DebChangelogFile
 
