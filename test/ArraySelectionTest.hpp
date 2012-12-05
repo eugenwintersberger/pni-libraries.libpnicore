e6c1d47d4bb5171c275d5b777cad3f49897e448a
Eugen Wintersberger <eugen.wintersberger@desy.de>
2012-12-05 12:41:45 +0100

    Added license headers.

--- test/ArraySelectionTest.hpp
+++ test/ArraySelectionTest.hpp
@@ -1,5 +1,26 @@
-#ifndef __ARRAYSELECTIONTEST_HPP__
-#define __ARRAYSELECTIONTEST_HPP__
+/*
+ * (c) Copyright 2012 DESY, Eugen Wintersberger <eugen.wintersberger@desy.de>
+ *
+ * This file is part of libpniutils.
+ *
+ * libpniutils is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * libpniutils is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libpniutils.  If not, see <http://www.gnu.org/licenses/>.
+ *************************************************************************
+ *
+ *  Created on: Jul 13, 2012
+ *      Author: Eugen Wintersberger <eugen.wintersberger@desy.de>
+ */
+#pragma once
 
 #include<cppunit/TestFixture.h>
 #include<cppunit/extensions/HelperMacros.h>
@@ -26,5 +47,3 @@ public:
 	void test_index();
 
 };
-
-#endif
