dea797dd8ee77f21d46bcc44c727e600bf3aded1
Eugen Wintersberger <eugen.wintersberger@desy.de>
2012-12-05 12:47:03 +0100

    Removed old benchmark code and added licence headers to further test sources.

--- test/BinaryTest.hpp
+++ test/BinaryTest.hpp
@@ -1,12 +1,34 @@
-//Unit test for the binary object
-
+/*
+ * (c) Copyright 2012 DESY, Eugen Wintersberger <eugen.wintersberger@desy.de>
+ *
+ * This file is part of libpniutils.
+ *
+ * libpniutils is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * libpniutils is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libpniutils.  If not, see <http://www.gnu.org/licenses/>.
+ *************************************************************************
+ *
+ *  Created on: Feb 08, 2012
+ *      Author: Eugen Wintersberger <eugen.wintersberger@desy.de>
+ */
+#pragma once
 #include<cppunit/TestFixture.h>
 #include<cppunit/extensions/HelperMacros.h>
 
 
 using namespace pni::utils;
 
-class BinaryTest : public CppUnit::TestFixture{
+class BinaryTest : public CppUnit::TestFixture
+{
 	CPPUNIT_TEST_SUITE(BinaryTest);
     CPPUNIT_TEST(test_construction);
     CPPUNIT_TEST(test_comparison);
--- test/BufferHelper.hpp
+++ test/BufferHelper.hpp
@@ -1,5 +1,26 @@
-#ifndef __BUFFERHELPER_HPP__
-#define __BUFFERHELPER_HPP__
+/*
+ * (c) Copyright 2012 DESY, Eugen Wintersberger <eugen.wintersberger@desy.de>
+ *
+ * This file is part of libpniutils.
+ *
+ * libpniutils is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * libpniutils is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libpniutils.  If not, see <http://www.gnu.org/licenses/>.
+ *************************************************************************
+ *
+ *  Created on: May 22, 2012
+ *      Author: Eugen Wintersberger <eugen.wintersberger@desy.de>
+ */
+#pragma once
 
 #include <pni/utils/DBuffer.hpp>
 #include <pni/utils/RBuffer.hpp>
@@ -7,6 +28,7 @@
 
 using namespace pni::utils;
 
+//-----------------------------------------------------------------------------
 template<typename T,typename Allocator>
     void allocate_buffer(DBuffer<T,Allocator> &b,size_t n)
 {
@@ -14,10 +36,9 @@ template<typename T,typename Allocator>
 }
 
 
-//-------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 template<typename T>
     void allocate_buffer(RBuffer<T> &b,size_t n)
 {
     b = RBuffer<T>(n,new T[n]);
 }
-#endif
