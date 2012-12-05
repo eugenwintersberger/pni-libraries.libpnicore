d38a4dde3b027b67742a1c4fe541e1998ec3b159
Eugen Wintersberger <eugen.wintersberger@desy.de>
2012-12-05 13:44:58 +0100

    Fixed license headers.

--- test/ResultTypeTraitTest.hpp
+++ test/ResultTypeTraitTest.hpp
@@ -1,6 +1,28 @@
-#ifndef __RESULTTYPETRAITTEST_HPP__
-#define __RESULTTYPETRAITTEST_HPP__
+/*
+ * (c) Copyright 2012 DESY, Eugen Wintersberger <eugen.wintersberger@desy.de>
+ *
+ * This file is part of libpniutils.
+ *
+ * libpniutils is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * libpniutils is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libpniutils.  If not, see <http://www.gnu.org/licenses/>.
+ *************************************************************************
+ *
+ *  Created on: Jul 25, 2012
+ *      Author: Eugen Wintersberger <eugen.wintersberger@desy.de>
+ */
+#pragma once
 
+#include <boost/current_function.hpp>
 #include<cppunit/TestFixture.h>
 #include<cppunit/extensions/HelperMacros.h>
 #include <pni/utils/ResultTypeTrait.hpp>
@@ -28,17 +50,20 @@ class ResultTypeTraitTest : public CppUnit::TestFixture
 };
 
 
-template<typename T> void ResultTypeTraitTest<T>::setUp()
-{
-}
+//-----------------------------------------------------------------------------
+template<typename T> void ResultTypeTraitTest<T>::setUp() { }
 
+//-----------------------------------------------------------------------------
 template<typename T> void ResultTypeTraitTest<T>::tearDown()
 {}
 
 
+//-----------------------------------------------------------------------------
 template<typename T> 
 template<typename U> void ResultTypeTraitTest<T>::test_add()
 {
+    std::cout<<BOOST_CURRENT_FUNCTION<<std::endl;
+
     typedef typename ResultTypeTrait<T,U>::add_type result_type;
 
     //now - if we add these two numbers the result should not be convineant for
@@ -55,5 +80,3 @@ template<typename U> void ResultTypeTraitTest<T>::test_add()
     CPPUNIT_ASSERT_THROW(convert_type<T>(t),RangeError);
     CPPUNIT_ASSERT_THROW(convert_type<U>(t),RangeError);
 }
-
-#endif
