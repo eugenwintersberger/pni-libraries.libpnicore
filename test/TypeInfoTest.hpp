23562a01fb8e3783641ede74824985899a4d6289
Eugen Wintersberger <eugen.wintersberger@desy.de>
2012-12-05 14:06:23 +0100

    Fixed license headers and added file creation dates.

--- test/TypeInfoTest.hpp
+++ test/TypeInfoTest.hpp
@@ -1,6 +1,26 @@
-
-#ifndef __TYPEINFOTEST_HPP__
-#define __TYPEINFOTEST_HPP__
+/*
+ * (c) Copyright 2012 DESY, Eugen Wintersberger <eugen.wintersberger@desy.de>
+ *
+ * This file is part of libpniutils.
+ *
+ * libpniutils is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * libpniutils is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libpniutils.  If not, see <http://www.gnu.org/licenses/>.
+ *************************************************************************
+ *
+ *  Created on: Oct 01, 2012
+ *      Author: Eugen Wintersberger <eugen.wintersberger@desy.de>
+ */
+#pragma once
 
 #include <boost/current_function.hpp>
 #include<cppunit/TestFixture.h>
@@ -86,5 +106,3 @@ template<typename T> void TypeInfoTest::test_float()
     CPPUNIT_ASSERT(info_t::is_negative(-1.1232));
     CPPUNIT_ASSERT(!info_t::is_negative(2.342));
 }
-
-#endif
