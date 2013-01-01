a21cea0f0340c00a883a74b915ad0cf4a73dcf66
Eugen Wintersberger <eugen.wintersberger@gmail.com>
2013-01-01 18:10:32 +0100

    Added hardware support.
    Added support for functions to investigate the hardware on which a program is
    running. This should be particularly useful for multi-threaded applications that
    need sufficient information about how many threads should be fired.

---
+++ src/hardware/cpu.hpp
@@ -0,0 +1,47 @@
+/*
+ * (c) Copyright 2012 DESY, Eugen Wintersberger <eugen.wintersberger@desy.de>
+ *
+ * This file is part of libpniutils.
+ *
+ * libpniutils is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * libpniutils is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libpniutils.  If not, see <http://www.gnu.org/licenses/>.
+ *************************************************************************
+ *
+ * Created on: Jan 01, 2013
+ *     Author: Eugen Wintersberger <eugen.wintersberger@desy.de>
+ */
+
+#pragma once
+
+namespace pni{
+namespace utils{
+   
+    /*!
+    \brief cpu class
+
+    This class represents a particular cpu. 
+
+    */
+    class cpu
+    {
+        private:
+            size_t _cache_size;
+            size_t _socket_id;
+            size_t 
+        
+    };
+
+
+//end of namespace
+}
+}
