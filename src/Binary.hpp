46bb9f3901b9d4eb80ffb5b911105406050ee9e5
Eugen Wintersberger <eugen.wintersberger@gmail.com>
2013-01-05 19:00:42 +0100

    Fixed license headers.

--- src/Binary.hpp
+++ src/Binary.hpp
@@ -1,20 +1,20 @@
 /*
  * (c) Copyright 2011 DESY, Eugen Wintersberger <eugen.wintersberger@desy.de>
  *
- * This file is part of libpniutils.
+ * This file is part of libpnicore.
  *
- * libpniutils is free software: you can redistribute it and/or modify
+ * libpnicore is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
  *
- * libpniutils is distributed in the hope that it will be useful,
+ * libpnicore is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with libpniutils.  If not, see <http://www.gnu.org/licenses/>.
+ * along with libpnicore.  If not, see <http://www.gnu.org/licenses/>.
  *************************************************************************
  *
  * Definition of a dynamic array template.
@@ -40,7 +40,7 @@ namespace core{
     for function overloading or for template specialization - it would always
     be treated by the compiler as the underlying unsigned char type.    
 
-    To circumvent this problem \c libpniutils defines this binary type which can
+    To circumvent this problem \c libpnicore defines this binary type which can
     be used instead of a simple typedef to unsigned char. The BinaryType
     template has several advantages: 
 
--- src/config/cli_args.cpp
+++ src/config/cli_args.cpp
@@ -1,14 +1,14 @@
 /*
  * (c) Copyright 2012 DESY, Eugen Wintersberger <eugen.wintersberger@desy.de>
  *
- * This file is part of pnitools.
+ * This file is part of libpnicore.
  *
- * pnitools is free software: you can redistribute it and/or modify
+ * libpnicore is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
  *
- * pnitools is distributed in the hope that it will be useful,
+ * libpnicore is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
