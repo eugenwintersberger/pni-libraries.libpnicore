b05616e555d11eb7bc1716c225a1e21b02e2b4ce
Eugen Wintersberger <eugen.wintersberger@gmail.com>
2013-02-04 22:22:10 +0100

    Fixed documentation of value.

--- src/value.hpp
+++ src/value.hpp
@@ -177,8 +177,10 @@ namespace core{
             */
             type_id_t type_id() const;
 
-            friend std::ostream &operator<<(std::ostream &,const value &);
-            friend std::istream &operator>>(std::istream &,value &);
+            friend std::ostream &operator<<(std::ostream &stream,
+                                            const value &v);
+            friend std::istream &operator>>(std::istream &stream,
+                                            value &v);
     };
 
     //=====================implementation of template member functions=========
