ab3ff5c8858f04f03e279424c36632ef1ff195ac
Eugen Wintersberger <eugen.wintersberger@gmail.com>
2013-10-28 15:14:12 +0100

    First sccessful test for mdarray

--- include/pni/core/index_map/index_maps.hpp
+++ include/pni/core/index_map/index_maps.hpp
@@ -28,14 +28,101 @@
 
 namespace pni{
 namespace core{
-    //define a C index policy
+    /*!
+    \brief a C index policy
+    
+    This is the default C index policy. Use this to define index maps which
+    should handle arrays in row major ordering (where the last index varies
+    fasters).
+    */
     typedef index_policy<c_index_policy_imp> c_index_policy;
 
-    //this header file defines some default index maps
-    template<size_t... DIMS> using static_cindex_map = static_index_map<c_index_policy,DIMS...>;
+    //-------------------------------------------------------------------------
+    /*!
+    \brief template for  a static C map
+   
+    A template alias for a static index map with c ordering. To define the index
+    map for a 3x3 matrix use something like this
+    \code
+    static_cindex_map<3,3> matrix_map; 
+    \endcode
 
-    //define the default dynamic C-index map
+    \tparam DIMS number of elements along each dimension
+    */
+    template<size_t... DIMS> 
+    using static_cindex_map = static_index_map<c_index_policy,DIMS...>;
+
+    //-------------------------------------------------------------------------
+    /*!
+    \brief definition of a dynamic C index map
+    
+    Type definition of a fully dynamic C index map. The structure of the map can
+    be fully determined at runtime. The structure of an array.
+    */
+    typedef index_map<std::vector<size_t>,c_index_policy> dynamic_cindex_map;
     typedef index_map<std::vector<size_t>,c_index_policy> cindex_map;
+
+    //-------------------------------------------------------------------------
+    /*!
+    \brief fixed dimension dynamic C index map
+
+    For such a map the number of elements along each dimension can be changed,
+    however the total size (rank) of the map cannot be changed once the concrete
+    type has been defined. 
+    A typical example would be to define a map for an image which has always a
+    rank of 2 like this
+    \code
+    typedef fixed_dim_cindex_map<2> image_map; 
+    \endcode
+
+    \tparam NDIMS number of dimensions
+    */
+    template<size_t NDIMS> 
+    using fixed_dim_cindex_map = index_map<std::array<size_t,NDIMS>,c_index_policy>;
+
+    //=================define some convienance function========================
+
+    template<typename MAPT> struct map_utils
+    {
+        template<typename CTYPE> static MAPT create(const CTYPE &c)
+        {
+            typename MAPT::storage_type storage(c.size());
+            std::copy(c.begin(),c.end(),storage.begin());
+            return MAPT(storage); 
+        }
+    };
+
+    //-------------------------------------------------------------------------
+    template<typename T,size_t NDIMS,typename POLTYPE> 
+    struct map_utils<index_map<std::array<T,NDIMS>,POLTYPE> >
+    {
+        typedef index_map<std::array<T,NDIMS>,POLTYPE> map_type;
+        template<typename CTYPE> static map_type create(const CTYPE &c)
+        {
+            typename map_type::storage_type storage;
+            std::copy(c.begin(),c.end(),storage.begin());
+            return map_type(storage);
+        }
+
+    };
+
+    //-------------------------------------------------------------------------
+    template<typename POLTYPE,size_t...DIMS>
+    struct map_utils<static_index_map<POLTYPE,DIMS...> >
+    {
+        typedef static_index_map<POLTYPE,DIMS...> map_type;
+        template<typename CTYPE> static map_type create(const CTYPE &c)
+        {
+        }
+        
+    };
+
+
+
+
+
+
+     
 
 //end of namespace
 }
---
+++ test/arrays/mdarray_test.hpp
@@ -0,0 +1,243 @@
+/*
+ * (c) Copyright 2013 DESY, Eugen Wintersberger <eugen.wintersberger@desy.de>
+ *
+ * This file is part of libpnicore.
+ *
+ * libpnicore is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * libpnicore is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libpnicore.  If not, see <http://www.gnu.org/licenses/>.
+ *************************************************************************
+ *
+ *  Created on: Oct 28, 2013
+ *      Author: Eugen Wintersberger <eugen.wintersberger@desy.de>
+ */
+#pragma once
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include <boost/current_function.hpp>
+#include <algorithm>
+
+#include "common.hpp"
+
+#include "../data_generator.hpp"
+#include "../uniform_distribution.hpp"
+#include "../EqualityCheck.hpp"
+
+using namespace pni::core;
+
+/*!
+\brief darray template test
+
+\tparam STORAGE type
+\tparam MAPT index map type
+ */
+template<typename ATYPE>
+class mdarray_test : public CppUnit::TestFixture
+{
+        CPPUNIT_TEST_SUITE(mdarray_test);
+        CPPUNIT_TEST(test_linear_access_operator);
+        CPPUNIT_TEST(test_linear_access_at);
+        CPPUNIT_TEST(test_linear_access_iterators);
+        CPPUNIT_TEST(test_linear_access_reverse_iterators);
+        CPPUNIT_TEST(test_multiindex_access_operator);
+        CPPUNIT_TEST(test_inquery);
+        CPPUNIT_TEST_SUITE_END();
+    private:
+        typedef typename ATYPE::map_type     index_map_type;
+        typedef typename ATYPE::storage_type storage_type;
+        typedef typename ATYPE::value_type   value_type;
+        std::vector<value_type> ref_data; //buffer with reference data
+        ATYPE array;
+        shape_t shape;
+
+        //---------------------------------------------------------------------
+        template<typename CTYPE> 
+        static void allocate_storage(CTYPE &s,size_t n)
+        {
+            s = CTYPE(n);
+        }
+
+        //---------------------------------------------------------------------
+        template<typename T,size_t N>
+        static void allocate_storage(std::array<T,N> &c,size_t n)
+        {
+        }
+    public:
+        void setUp();
+        void tearDown();
+        void test_linear_access_operator();
+        void test_linear_access_at();
+        void test_linear_access_iterators();
+        void test_linear_access_reverse_iterators();
+        void test_reverse_iterators();
+        void test_multiindex_access_operator();
+        void test_inquery();
+};
+
+//------------------------------------------------------------------------------
+template<typename ATYPE> void mdarray_test<ATYPE>::setUp()
+{
+    shape = {2,3,5};
+    ref_data = std::vector<value_type>(2*3*5);
+    
+    //create reference data 
+    std::iota(ref_data.begin(),ref_data.end(),0.);    
+
+    //create the target array
+    auto map = map_utils<index_map_type>::create(shape);
+    storage_type storage;
+    allocate_storage(storage,map.max_elements());
+    array = ATYPE(map,storage);
+}
+
+//------------------------------------------------------------------------------
+template<typename ATYPE> void mdarray_test<ATYPE>::tearDown() { }
+
+//------------------------------------------------------------------------------
+template<typename ATYPE> void mdarray_test<ATYPE>::test_inquery()
+{
+    std::cout<<BOOST_CURRENT_FUNCTION<<std::endl;
+
+    CPPUNIT_ASSERT(array.rank() == 3);
+    CPPUNIT_ASSERT(array.size() == 30);
+    CPPUNIT_ASSERT(ATYPE::type_id == type_id_map<value_type>::type_id);
+}
+
+//------------------------------------------------------------------------------
+template<typename ATYPE> 
+void mdarray_test<ATYPE>::test_linear_access_operator()
+{
+    std::cout<<BOOST_CURRENT_FUNCTION<<std::endl;
+
+    //write data to the array
+    for(size_t i=0;i<array.size();++i) array[i] = ref_data[i];
+
+    //read data back
+    for(size_t i=0;i<array.size();++i) check_equality(array[i],ref_data[i]);
+
+    //test with constant access
+    const ATYPE &ca = array;
+
+    //read data back
+    for(size_t i=0;i<array.size();++i) check_equality(array[i],ref_data[i]);
+}
+
+//------------------------------------------------------------------------------
+template<typename ATYPE>
+void mdarray_test<ATYPE>::test_linear_access_at()
+{
+    std::cout<<BOOST_CURRENT_FUNCTION<<std::endl;
+    
+    //write data to the array
+    for(size_t i=0;i<array.size();++i) array.at(i) = ref_data.at(i);
+
+    //read data back
+    for(size_t i=0;i<array.size();++i) 
+        check_equality(array.at(i),ref_data.at(i));
+
+    CPPUNIT_ASSERT_THROW(array.at(array.size()),index_error);
+
+    //test with constant access
+    const ATYPE &ca = array;
+
+    //read data back
+    for(size_t i=0;i<ca.size();++i) 
+        check_equality(ca.at(i),ref_data.at(i));
+    
+    CPPUNIT_ASSERT_THROW(ca.at(array.size()),index_error);
+}
+
+//------------------------------------------------------------------------------
+template<typename ATYPE> 
+void mdarray_test<ATYPE>::test_linear_access_iterators()
+{
+    std::cout<<BOOST_CURRENT_FUNCTION<<std::endl;
+    
+    auto aiter = array.begin();
+    auto viter = ref_data.begin();
+
+    //write data
+    for(;aiter!=array.end();++aiter,++viter)
+        *aiter = *viter;
+   
+    //read data back
+    aiter = array.begin();
+    viter = ref_data.begin();
+    for(;aiter!=array.end();++aiter,++viter)
+        check_equality(*aiter,*viter);
+
+    //test with constant iterator
+    const ATYPE &ca = array;
+    auto caiter = ca.begin();
+    viter = ref_data.begin();
+    for(;caiter != ca.end();++caiter,++viter)
+        check_equality(*caiter,*viter);
+}
+
+//------------------------------------------------------------------------------
+template<typename ATYPE>
+void mdarray_test<ATYPE>::test_linear_access_reverse_iterators()
+{
+    std::cout<<BOOST_CURRENT_FUNCTION<<std::endl;
+    
+    auto aiter = array.rbegin();
+    auto viter = ref_data.rbegin();
+
+    //write data
+    for(;aiter!=array.rend();++aiter,++viter)
+        *aiter = *viter;
+   
+    //read data back
+    aiter = array.rbegin();
+    viter = ref_data.rbegin();
+    for(;aiter!=array.rend();++aiter,++viter)
+        check_equality(*aiter,*viter);
+
+    //test with constant iterator
+    const ATYPE &ca = array;
+    auto caiter = ca.rbegin();
+    viter = ref_data.rbegin();
+    for(;caiter != ca.rend();++caiter,++viter)
+        check_equality(*caiter,*viter);
+}
+
+//-----------------------------------------------------------------------------
+template<typename ATYPE> 
+void mdarray_test<ATYPE>::test_multiindex_access_operator()
+{   
+    std::cout<<BOOST_CURRENT_FUNCTION<<std::endl;
+
+    //write data
+    auto viter = ref_data.begin();
+    for(size_t i=0;i<shape[0];++i)
+        for(size_t j=0;j<shape[1];++j)
+            for(size_t k=0;k<shape[2];++k)
+                array(i,j,k) = *viter++;
+
+    //read data back
+    viter = ref_data.begin();
+    for(size_t i=0;i<shape[0];++i)
+        for(size_t j=0;j<shape[1];++j)
+            for(size_t k=0;k<shape[2];++k)
+                check_equality(array(i,j,k),*viter++);
+
+    //test with a const reference
+    viter = ref_data.begin();
+    const ATYPE &ca = array;
+    for(size_t i=0;i<shape[0];++i)
+        for(size_t j=0;j<shape[1];++j)
+            for(size_t k=0;k<shape[2];++k)
+                check_equality(array(i,j,k),*viter++);
+
+}
+
+
