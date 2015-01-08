3e6b1509e0ee373f7fe3a608a5b96f9ee130c49e
Eugen Wintersberger <eugen.wintersberger@gmail.com>
2015-01-08 13:04:50 +0100

    First successful test of the new as<> function template.

--- include/pni/core/type_erasures/as.hpp
+++ include/pni/core/type_erasures/as.hpp
@@ -28,80 +28,120 @@
 #include "../types/types.hpp"
 #include "../types/convert.hpp"
 #include "../types/type_conversion.hpp"
-
+#include "../types/traits.hpp"
+#include "value.hpp"
 
 namespace pni{
 namespace core{
 
+
     template<
-             typename T,
-             typename V
+             typename TT,
+             typename ST,
+             bool     convertible=false
             > 
-    T as(const V &v)
+    struct value_converter
+    {
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+        static TT convert(const ST &v)
+        {
+            throw type_error(EXCEPTION_RECORD,"Conversion not possible!");
+            return TT();
+        }
+#pragma GCC diagnostic pop
+    };
+
+    template<
+             typename TT,
+             typename ST
+            >
+    struct value_converter<TT,ST,true>
+    {
+        static TT convert(const ST &v)
+        {
+            return pni::core::convert<TT>(v);
+        }
+    };
+
+    template<
+             typename TT,
+             typename ST
+            >
+    using strategy = value_converter<TT,ST,convertible<ST,TT>::value>;
+
+    template<
+             typename T
+            > 
+    T as(const value &v)
     {
         //type ID of the type erasure
         type_id_t source_tid = v.type_id();
         //type ID of the requested type
         type_id_t target_tid = type_id_map<T>::type_id;
 
-        if(is_numeric(source_tid))
+        //check if the type are convertible at all
+        if(!is_convertible(source_tid,target_tid))
+            throw type_error(EXCEPTION_RECORD,"Types are not convertible!");
+
+        switch(source_tid)
         {
-            if(!is_convertible(source_tid,target_tid))
-                throw type_error(EXCEPTION_RECORD,
-                        "Type conversion not possible!");
-
-            if(source_tid == type_id_t::UINT8)
-                return convert<T>(v.template as<uint8>());
-            else if(source_tid == type_id_t::INT8)
-                return convert<T>(v.template as<int8>());
-            else if(source_tid == type_id_t::UINT16)
-                return convert<T>(v.template as<uint16>());
-            else if(source_tid == type_id_t::INT16)
-                return convert<T>(v.template as<int16>());
-            else if(source_tid == type_id_t::UINT32)
-                return convert<T>(v.template as<uint32>());
-            else if(source_tid == type_id_t::INT32)
-                return convert<T>(v.template as<int32>());
-            else if(source_tid == type_id_t::UINT64)
-                return convert<T>(v.template as<uint64>());
-            else if(source_tid == type_id_t::INT64)
-                return convert<T>(v.template as<int64>());
-            else if(source_tid == type_id_t::FLOAT32)
-                return convert<T>(v.template as<float32>());
-            else if(source_tid == type_id_t::FLOAT64)
-                return convert<T>(v.template as<float64>());
-            else if(source_tid == type_id_t::FLOAT128)
-                return convert<T>(v.template as<float128>());
-            else if(source_tid == type_id_t::COMPLEX32)
-                return convert<T>(v.template as<complex32>());
-            else if(source_tid == type_id_t::COMPLEX64)
-                return convert<T>(v.template as<complex64>());
-            else if(source_tid == type_id_t::COMPLEX128)
-                return convert<T>(v.template as<complex128>());
-            else
-                throw type_error(EXCEPTION_RECORD,"Unknown type ID");
-        }
-        else
-        {
-            //for non numeric types the type IDs must be equal
-            if(source_tid != target_tid)
-                throw type_error(EXCEPTION_RECORD,
-                        "Types must match for non-numeric types!");
-
-
-            if(source_tid == type_id_t::STRING)
-                return v.template as<string>();
-            else if(source_tid == type_id_t::BINARY)
-                return v.template as<binary>();
-            else if(source_tid == type_id_t::BOOL)
-                return v.template as<bool_t>();
-            else 
+            case type_id_t::UINT8:   
+                return strategy<T,uint8>::convert(v.as<uint8>());
+            case type_id_t::INT8:    
+                return strategy<T,int8>::convert(v.as<int8>());
+            case  type_id_t::UINT16: 
+                return strategy<T,uint16>::convert(v.as<uint16>());
+            case type_id_t::INT16:
+                return strategy<T,int16>::convert(v.as<int16>());
+            case type_id_t::UINT32:
+                return strategy<T,uint32>::convert(v.as<uint32>());
+            case type_id_t::INT32:
+                return strategy<T,int32>::convert(v.as<int32>());
+            case type_id_t::UINT64:
+                return strategy<T,uint64>::convert(v.as<uint64>());
+            case type_id_t::INT64:
+                return strategy<T,int64>::convert(v.as<int64>());
+            case type_id_t::FLOAT32:
+                return strategy<T,float32>::convert(v.as<float32>());
+            case type_id_t::FLOAT64:
+                return strategy<T,float64>::convert(v.as<float64>());
+            case type_id_t::FLOAT128:
+                return strategy<T,float128>::convert(v.as<float128>());
+            case type_id_t::COMPLEX32:
+                return strategy<T,complex32>::convert(v.as<complex32>());
+            case type_id_t::COMPLEX64:
+                return strategy<T,complex64>::convert(v.as<complex64>());
+            case type_id_t::COMPLEX128:
+                return strategy<T,complex128>::convert(v.as<complex128>());
+                /*
+            case type_id_t::STRING:
+                if(target_tid == source_tid)
+                    return v.as<string>();
+                else
+                    throw type_error(EXCEPTION_RECORD,
+                            "Source and target type must match for strings!");
+            case type_id_t::BINARY:
+                if(target_tid == source_tid)
+                    return v.as<binary>();
+                else 
+                    throw type_error(EXCEPTION_RECORD,
+                            "Source and target type must match for binary data!");
+            case type_id_t::BOOL:
+                if(target_tid == source_tid)
+                    return v.as<bool_t>();
+                else
+                    throw type_error(EXCEPTION_RECORD,
+                            "Source and target type must match for bool data!");
+                            */
+            default: 
                 throw type_error(EXCEPTION_RECORD,"Unkown type ID!");
-
-
         }
-
     }
+
+    template<> string as(const value &v);
+    template<> binary as(const value &v);
+    template<> bool_t as(const value &v);
 
 //end of namespace
 }
--- test/type_erasures/value_test.hpp
+++ test/type_erasures/value_test.hpp
@@ -155,7 +155,6 @@ template<typename T> void value_test<T>::test_comparison()
     value v2(value_2);
     CPPUNIT_ASSERT((v1==v1) == (value_1==value_1));
     CPPUNIT_ASSERT((v2!=v1) == (value_1!=value_2));
-
 }
 
 
