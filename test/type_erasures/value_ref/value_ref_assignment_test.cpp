62aeea76abe530b9d2f9fc0a54c70adb4644d286
Eugen Wintersberger <eugen.wintersberger@gmail.com>
2015-10-19 16:08:23 +0200

    value_ref test uses now boost::unit_test

--- test/type_erasures/value_ref/value_ref_assignment_test.cpp
+++ test/type_erasures/value_ref/value_ref_assignment_test.cpp
@@ -1,48 +1,87 @@
-//!
-//! (c) Copyright 2015 DESY, Eugen Wintersberger <eugen.wintersberger@desy.de>
-//!
-//! This file is part of libpnicore.
-//!
-//! libpnicore is free software: you can redistribute it and/or modify
-//! it under the terms of the GNU General Public License as published by
-//! the Free Software Foundation, either version 2 of the License, or
-//! (at your option) any later version.
-//!
-//! libpnicore is distributed in the hope that it will be useful,
-//! but WITHOUT ANY WARRANTY; without even the implied warranty of
-//! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//! GNU General Public License for more details.
-//!
-//! You should have received a copy of the GNU General Public License
-//! along with libpnicore.  If not, see <http://www.gnu.org/licenses/>.
-//!
-//! ===========================================================================
-//!
-//!  Created on: Jan 9, 2015
-//!      Author: Eugen Wintersberger <eugen.wintersberger@desy.de>
-//!
+//
+// (c) Copyright 2015 DESY, Eugen Wintersberger <eugen.wintersberger@desy.de>
+//
+// This file is part of libpnicore.
+//
+// libpnicore is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// libpnicore is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with libpnicore.  If not, see <http://www.gnu.org/licenses/>.
+//
+// ===========================================================================
+//
+//  Created on: Jan 9, 2015
+//      Author: Eugen Wintersberger <eugen.wintersberger@desy.de>
+//
 
-#include <boost/current_function.hpp>
-#include<cppunit/extensions/HelperMacros.h>
+#include <boost/test/unit_test.hpp>
+#include <boost/test/floating_point_comparison.hpp>
 
-#include "value_ref_assignment_test.hpp"
+#include <pni/core/type_erasures.hpp>
+#include "../value/fixture.hpp"
+#include "../value/types.hpp"
 
-CPPUNIT_TEST_SUITE_REGISTRATION(value_ref_assignment_test<uint8>);
-CPPUNIT_TEST_SUITE_REGISTRATION(value_ref_assignment_test<int8>);
-CPPUNIT_TEST_SUITE_REGISTRATION(value_ref_assignment_test<uint16>);
-CPPUNIT_TEST_SUITE_REGISTRATION(value_ref_assignment_test<int16>);
-CPPUNIT_TEST_SUITE_REGISTRATION(value_ref_assignment_test<uint32>);
-CPPUNIT_TEST_SUITE_REGISTRATION(value_ref_assignment_test<int32>);
-CPPUNIT_TEST_SUITE_REGISTRATION(value_ref_assignment_test<uint64>);
-CPPUNIT_TEST_SUITE_REGISTRATION(value_ref_assignment_test<int64>);
+using namespace pni::core;
 
-CPPUNIT_TEST_SUITE_REGISTRATION(value_ref_assignment_test<float32>);
-CPPUNIT_TEST_SUITE_REGISTRATION(value_ref_assignment_test<float64>);
-CPPUNIT_TEST_SUITE_REGISTRATION(value_ref_assignment_test<float128>);
+BOOST_AUTO_TEST_SUITE(value_ref_assignment_test)
 
-CPPUNIT_TEST_SUITE_REGISTRATION(value_ref_assignment_test<complex32>);
-CPPUNIT_TEST_SUITE_REGISTRATION(value_ref_assignment_test<complex64>);
-CPPUNIT_TEST_SUITE_REGISTRATION(value_ref_assignment_test<complex128>);
+    BOOST_AUTO_TEST_CASE_TEMPLATE(test_copy_assignment,T,all_types)
+    {
+        fixture<T> f;
+        value_ref v1(std::ref(f.value_1)); 
+        value_ref v2(std::ref(f.value_2));
 
-CPPUNIT_TEST_SUITE_REGISTRATION(value_ref_assignment_test<string>);
-CPPUNIT_TEST_SUITE_REGISTRATION(value_ref_assignment_test<bool_t>);
+        v2 = v1; //now v2 referes to the same value - value_1
+
+        BOOST_CHECK_EQUAL(v1.as<T>(),f.value_1);
+        BOOST_CHECK_EQUAL(v2.as<T>(),f.value_1);
+
+        f.value_1 = f.generator();
+
+        BOOST_CHECK_EQUAL(v1.as<T>(),f.value_1);
+        BOOST_CHECK_EQUAL(v2.as<T>(),f.value_1);
+    }
+    
+    //========================================================================
+    BOOST_AUTO_TEST_CASE_TEMPLATE(test_assignment_from_value,T,all_types)
+    {
+        fixture<T> f;
+        value v(f.value_1);
+        value_ref ref(std::ref(f.value_2));
+    
+        ref = v;
+        BOOST_CHECK_EQUAL(ref.as<T>(),f.value_1);
+    }
+
+    //========================================================================
+    BOOST_AUTO_TEST_CASE_TEMPLATE(test_from_T,T,all_types)
+    {
+        fixture<T> f;
+        value_ref v(std::ref(f.value_1));
+        v = f.value_2;
+        BOOST_CHECK_EQUAL(v.as<T>(),f.value_2);
+        BOOST_CHECK_EQUAL(f.value_2,f.value_1);
+    }
+
+    //========================================================================
+    BOOST_AUTO_TEST_CASE_TEMPLATE(test_to_value,T,all_types)
+    {
+        fixture<T> f;
+        value_ref v_ref(std::ref(f.value_1));
+        value v = to_value(v_ref);
+        
+        BOOST_CHECK_EQUAL(v.as<T>(),v_ref.as<T>());
+        BOOST_CHECK_EQUAL(v.as<T>(),f.value_1);
+    }
+
+BOOST_AUTO_TEST_SUITE_END()
+
+
--- test/type_erasures/value_ref/value_ref_construction_test.cpp
+++ test/type_erasures/value_ref/value_ref_construction_test.cpp
@@ -1,48 +1,81 @@
-//!
-//! (c) Copyright 2015 DESY, Eugen Wintersberger <eugen.wintersberger@desy.de>
-//!
-//! This file is part of libpnicore.
-//!
-//! libpnicore is free software: you can redistribute it and/or modify
-//! it under the terms of the GNU General Public License as published by
-//! the Free Software Foundation, either version 2 of the License, or
-//! (at your option) any later version.
-//!
-//! libpnicore is distributed in the hope that it will be useful,
-//! but WITHOUT ANY WARRANTY; without even the implied warranty of
-//! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//! GNU General Public License for more details.
-//!
-//! You should have received a copy of the GNU General Public License
-//! along with libpnicore.  If not, see <http://www.gnu.org/licenses/>.
-//!
-//! ===========================================================================
-//!
-//!  Created on: Jan 9, 2015
-//!      Author: Eugen Wintersberger <eugen.wintersberger@desy.de>
-//!
+//
+// (c) Copyright 2015 DESY, Eugen Wintersberger <eugen.wintersberger@desy.de>
+//
+// This file is part of libpnicore.
+//
+// libpnicore is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// libpnicore is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with libpnicore.  If not, see <http://www.gnu.org/licenses/>.
+//
+// ===========================================================================
+//
+//  Created on: Jan 9, 2015
+//      Author: Eugen Wintersberger <eugen.wintersberger@desy.de>
+//
 
-#include <boost/current_function.hpp>
-#include<cppunit/extensions/HelperMacros.h>
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE testing the value_ref type earsure
+#include <boost/test/unit_test.hpp>
+#include <boost/test/floating_point_comparison.hpp>
 
-#include "value_ref_construction_test.hpp"
+#include <pni/core/types.hpp>
+#include <pni/core/type_erasures.hpp>
 
-CPPUNIT_TEST_SUITE_REGISTRATION(value_ref_construction_test<uint8>);
-CPPUNIT_TEST_SUITE_REGISTRATION(value_ref_construction_test<int8>);
-CPPUNIT_TEST_SUITE_REGISTRATION(value_ref_construction_test<uint16>);
-CPPUNIT_TEST_SUITE_REGISTRATION(value_ref_construction_test<int16>);
-CPPUNIT_TEST_SUITE_REGISTRATION(value_ref_construction_test<uint32>);
-CPPUNIT_TEST_SUITE_REGISTRATION(value_ref_construction_test<int32>);
-CPPUNIT_TEST_SUITE_REGISTRATION(value_ref_construction_test<uint64>);
-CPPUNIT_TEST_SUITE_REGISTRATION(value_ref_construction_test<int64>);
+#include "../value/fixture.hpp"
+#include "../value/types.hpp"
 
-CPPUNIT_TEST_SUITE_REGISTRATION(value_ref_construction_test<float32>);
-CPPUNIT_TEST_SUITE_REGISTRATION(value_ref_construction_test<float64>);
-CPPUNIT_TEST_SUITE_REGISTRATION(value_ref_construction_test<float128>);
+using namespace pni::core;
 
-CPPUNIT_TEST_SUITE_REGISTRATION(value_ref_construction_test<complex32>);
-CPPUNIT_TEST_SUITE_REGISTRATION(value_ref_construction_test<complex64>);
-CPPUNIT_TEST_SUITE_REGISTRATION(value_ref_construction_test<complex128>);
+BOOST_AUTO_TEST_SUITE(value_ref_construction_test)
 
-CPPUNIT_TEST_SUITE_REGISTRATION(value_ref_construction_test<string>);
-CPPUNIT_TEST_SUITE_REGISTRATION(value_ref_construction_test<bool_t>);
+    BOOST_AUTO_TEST_CASE(test_default_construction)
+    {
+        value_ref v;
+        BOOST_CHECK_THROW(v.as<double>(),memory_not_allocated_error);
+        BOOST_CHECK_THROW(v.type_id(),memory_not_allocated_error);
+
+    }
+
+    //========================================================================
+    BOOST_AUTO_TEST_CASE_TEMPLATE(test_from_value,T,all_types)
+    {
+        fixture<T> f;
+        value_ref v1(std::ref(f.value_1));
+        value_ref v2(std::ref(f.value_2));
+
+        BOOST_CHECK_EQUAL(v1.as<T>(),f.value_1);
+        BOOST_CHECK_EQUAL(v2.as<T>(),f.value_2);
+
+        f.value_1 = f.generator();
+        f.value_2 = f.generator();
+
+        BOOST_CHECK_EQUAL(v1.as<T>(),f.value_1);
+        BOOST_CHECK_EQUAL(v2.as<T>(),f.value_2);
+    }
+
+    //========================================================================
+    BOOST_AUTO_TEST_CASE_TEMPLATE(test_copy_construction,T,all_types)
+    {
+        fixture<T> f;
+        value_ref v1(std::ref(f.value_1));
+        value_ref v2(v1);
+        BOOST_CHECK_EQUAL(v1.as<T>(),v2.as<T>());
+
+        //change value_1
+        f.value_1 = f.generator();
+        BOOST_CHECK_EQUAL(v1.as<T>(),f.value_1);
+        BOOST_CHECK_EQUAL(v2.as<T>(),f.value_1);
+
+    }
+
+BOOST_AUTO_TEST_SUITE_END()
+
