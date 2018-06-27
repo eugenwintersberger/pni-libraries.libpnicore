1891d12612485ff2cfd7fc303e5085a13768482d
Jan Kotanski <jankotan@gmail.com>
2018-06-27 23:41:14 +0200

    workaround for MSC

--- test/types/type_info_test.cpp
+++ test/types/type_info_test.cpp
@@ -118,10 +118,10 @@ BOOST_AUTO_TEST_SUITE(type_info_test)
 
         BOOST_CHECK_CLOSE(info_type::min(),-limits_type::max(),1.e-12);
         BOOST_CHECK_CLOSE(info_type::max(),+limits_type::max(),1.e-12);
-#if defined(_MSC_VER) || defined(__arm__)
-	BOOST_CHECK(info_type::size == 8);
-#else
+#if defined(__GNUG__)  && !defined(__arm__)
         BOOST_CHECK(info_type::size==16);
+#else
+	BOOST_CHECK(info_type::size == 8);
 #endif
         BOOST_CHECK(!info_type::is_integer);
         BOOST_CHECK(info_type::is_signed);
@@ -164,10 +164,10 @@ BOOST_AUTO_TEST_SUITE(type_info_test)
 
         BOOST_CHECK_CLOSE(info_type::min(),-limits_type::max(),1.e-12);
         BOOST_CHECK_CLOSE(info_type::max(),+limits_type::max(),1.e-12);
-#if defined(_MSC_VER) || defined(__arm__)
-		BOOST_CHECK(info_type::size == 16);
-#else
+#if defined(__GNUG__)  && !defined(__arm__)
         BOOST_CHECK(info_type::size==32);
+#else
+	BOOST_CHECK(info_type::size == 16);
 #endif
         BOOST_CHECK(!info_type::is_integer);
         BOOST_CHECK(info_type::is_signed);
